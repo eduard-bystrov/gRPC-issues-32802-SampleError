9bf6a0629e5d5d4873286d44b6f3526c6162a619
Быстров Эдуард Андреевич <eduard.bystrov@monitel.com>
2023-04-05 15:13:55 +0300

    init error sample

---
+++ AnotherOneTestProject/UnitTest1.cs
@@ -0,0 +1,14 @@
+using Aggregate;
+using Xunit;
+
+namespace AnotherOneTestProject
+{
+	public class UnitTest1
+	{
+		[Fact]
+		public void Test1()
+		{
+			Aggregator.AggregatorClient _ = null;
+		}
+	}
+}
